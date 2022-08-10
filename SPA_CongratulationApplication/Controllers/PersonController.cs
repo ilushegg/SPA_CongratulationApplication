66b6c6973da443543fd95f0b00a48b21a7953d28
ilushegg <marnopolskiyi@mail.ru>
2022-08-10 13:04:07 +0300

    Added controlers

---
+++ SPA_CongratulationApplication/Controllers/PersonController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SPA_CongratulationApplication.Data;
+using SPA_CongratulationApplication.Models;
+
+namespace SPA_CongratulationApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PersonController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPeople()
+        {
+          if (_context.People == null)
+          {
+              return NotFound();
+          }
+            return await _context.People.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(int id)
+        {
+          if (_context.People == null)
+          {
+              return NotFound();
+          }
+            var person = await _context.People.FindAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson(int id, Person person)
+        {
+            if (id != person.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Person>> PostPerson(Person person)
+        {
+          if (_context.People == null)
+          {
+              return Problem("Entity set 'DataContext.People'  is null.");
+          }
+            _context.People.Add(person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new { id = person.id }, person);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            if (_context.People == null)
+            {
+                return NotFound();
+            }
+            var person = await _context.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PersonExists(int id)
+        {
+            return (_context.People?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+    }
+}
